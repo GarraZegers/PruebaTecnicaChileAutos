6b7eeaa4a9638d9ad9ff3f1de5eee1b6c52f84e0
Fernando Campos <fernando@gett.cl>
2025-06-13 12:01:46 -0400

    Primera carga y avance de DTOs y configuracion general del proyecto.

---
+++ backend/PruebaTecnicaChileautos.Core/DTOs/EpisodeDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaChileautos.Core.DTOs
+{
+    public class EpisodeDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("air_date")]
+        public string AirDate { get; set; } = string.Empty;
+
+        [JsonPropertyName("episode")]
+        public string Episode { get; set; } = string.Empty;
+
+        [JsonPropertyName("characters")]
+        public List<string> Characters { get; set; } = new();
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = string.Empty;
+
+        [JsonPropertyName("created")]
+        public string Created { get; set; } = string.Empty;
+    }
+}
